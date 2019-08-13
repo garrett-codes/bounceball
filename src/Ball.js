7f2e183a57c9d3ed20abc97115bc326a6b035d8c
Garrett Halstein <garrett.halstein@emory.edu>
2019-08-13 14:13:40 -0400

    added to css and and ball

---
+++ src/Ball.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Ball extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+				<div> Ball </div>
+			</React.Fragment>
+		)
+	}
+}
